9a917eb423333ba09afb07edec37ed6553e62cfc
Bartek Bugała <bartek.bugala@gmail.com>
2019-05-13 20:08:27 +0200

    modified:   css/style.css 	modified:   css/style.css.map 	modified:   index.html 	modified:   js/modals.js 	new file:   js/utils.js

--- js/modals.js
+++ js/modals.js
@@ -39,28 +39,3 @@
     }
 
 })(); 
-
-function generateGameScoreTable(scoreArray,columnNamesObject) {
-    let htmlString = '<table><thead><tr>'+generateColumnNames(columnNamesObject)+'</tr></thead><tbody>';
-    for (let i = 0; i < scoreArray.length; i++) {
-        htmlString += '<tr>'+generateGameScoreRowContent(scoreArray[i])+'</tr>'
-    }
-    htmlString += '</tbody></table>';
-    return htmlString;
-}
-
-function generateGameScoreRowContent(roundDataObject) {
-    let htmlString = '';
-    for (let key in roundDataObject) {
-        htmlString += '<td>' + roundDataObject[key] + '</td>';
-    }
-    return htmlString;
-}
-
-function generateColumnNames(columnNamesObject) {
-    let htmlString = '';
-    for (let key in columnNamesObject) {
-        htmlString += '<th>' + columnNamesObject[key] + '</th>';
-    }
-    return htmlString;
-}
